e7bf943d8481d4cc5599472e56323e393a7e7d9b
Kenny Kwan <kenny.kwan9@gmail.com>
2019-11-19 22:09:05 -0800

    bubble chart

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 function create(error, numericInfo) {
+    createBubbleGraph(numericInfo);
+    createBarChart(numericInfo);
+
+};
+
+
+function createBubbleGraph(numericInfo) {
     // set the dimensions and margins of the graph
     var width = 1000;
     var height = 800;
@@ -77,9 +84,10 @@ function create(error, numericInfo) {
         .on("mouseover", mouseover) // What to do when hovered
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
-        .on("click", function(d) {
+        .on("click", function (d) {
             console.log(d);
-            console.log(d3.select(this));
+            d3.select('#bubble_chart').style('display', 'none');
+
         })
         .call(d3.drag() // call specific function when circle is dragged
             .on("start", dragstarted)
@@ -117,4 +125,62 @@ function create(error, numericInfo) {
         d.fx = null;
         d.fy = null;
     }
-};
+}
+
+function createBarChart(numericInfo) {
+    data = numericInfo.data;
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    // set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#metric-chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    console.log(svg);
+
+    // // get the data
+    // if (error) throw error;
+
+    // // format the data
+    // data.forEach(function (d) {
+    //     d.sales = +d.sales;
+    // });
+
+    // Scale the range of the data in the domains
+    x.domain(data.map(function (d) { return d.methodName; }));
+    y.domain([0, d3.max(data, function (d) { return d.numLines; })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return x(d.methodName); })
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) { return y(d.numLines); })
+        .attr("height", function (d) { return height - y(d.numLines); });
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+}
