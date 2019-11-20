004d9bd6b089b2838653537196d8a2724d1cbd12
Kenny Kwan <kenny.kwan9@gmail.com>
2019-11-20 15:07:39 -0800

    network graph linked and class name displays on hover

--- src/graph.js
+++ src/graph.js
@@ -9,6 +9,7 @@ function create(error, classMetrics) {
 function createNetwork(data) {
   var centerNode = {};
   centerNode.id = data.length + 1;
+  centerNode.classSize = 0;
   data.push(centerNode);
 
   var links = createCenterLinks(data);
@@ -18,6 +19,28 @@ function createNetwork(data) {
     width = 1000 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
 
+  // Color palette for nodes
+  var color = function (d) {
+    // Center Neutral Node
+    if (!d.hasOwnProperty("codeSmellScore")) {
+      return "#858585";
+      // Red Nodes
+    } else if (d.codeSmellScore > 50) {
+      return "#F96969";
+      // Yellow Nodes
+    } else if (d.codeSmellScore > 30) {
+      return "#F9F069";
+      // Green Nodes
+    } else {
+      return "#99F969";
+    }
+  }
+
+  // Size scale for countries
+  var size = d3.scaleLinear()
+    .domain([0, 5000])
+    .range([15, 55])  // circle will be between 15 and 120 px wide
+
   // append the svg object to the body of the page
   var svg = d3.select("#network_graph")
     .append("svg")
@@ -26,6 +49,39 @@ function createNetwork(data) {
     .append("g")
     .attr("transform",
       "translate(" + margin.left + "," + margin.top + ")");
+
+  // create a tooltip
+  var Tooltip = d3.select("#network_graph")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .style("pointer-events", "none")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function (d) {
+    console.log("over");
+    Tooltip
+      .html('<u>' + d.className + '</u>')
+      .style("left", (d3.mouse(this)[0] + 20) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+      .style("opacity", 1)
+      .style("visibility", "visible")
+  }
+
+  var mouseleave = function (d) {
+    console.log("leave");
+    Tooltip
+      .style("opacity", 0)
+      .style("visibility", "hidden")
+  }
 
   // Initialize the links
   var link = svg
@@ -41,33 +97,34 @@ function createNetwork(data) {
     .data(data)
     .enter()
     .append("circle")
-    .attr("r", 20)
-    .style("fill", "#69b3a2")
+    .attr("r", function (d) { return size(d.classSize) })
+    .style("fill", function (d) { return color(d) })
+    .on("mouseenter", mouseover)
+    .on("mouseleave", mouseleave)
+
+
 
   // Let's list the force we wanna apply on the network
   var simulation = d3.forceSimulation(data)                   // Force algorithm is applied to data.nodes
     .force("link", d3.forceLink()                             // This force provides links between nodes
       .id(function (d) { return d.id; })                      // This provide  the id of a node
       .links(links)                                           // and this the list of links
-      )
-    .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+    )
+    .force("charge", d3.forceManyBody().strength(-4000))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
     .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
     .on("end", ticked);
 
   // This function is run at each iteration of the force algorithm, updating the nodes position.
   function ticked() {
     link
-      .attr("x1", function (d) { 
-        return d.source.x; })
+      .attr("x1", function (d) { return d.source.x; })
       .attr("y1", function (d) { return d.source.y; })
       .attr("x2", function (d) { return d.target.x; })
       .attr("y2", function (d) { return d.target.y; });
 
     node
-      .attr("cx", function (d) {
-        return d.x + 6;
-      })
-      .attr("cy", function (d) { return d.y - 6; });
+      .attr("cx", function (d) { return d.x; })
+      .attr("cy", function (d) { return d.y });
   }
 }
 
