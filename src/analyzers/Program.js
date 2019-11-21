2acc324bb4a0f013346c434aa7e2fb62972b1149
sqhsb <sadeqhsb@tuta.io>
2019-11-21 14:45:36 -0800

    add depth info to object and update Program.js

--- src/analyzers/Program.js
+++ src/analyzers/Program.js
@@ -1,18 +1,22 @@
-import * as na from "./Utils.js"
+import * as na from './NestingAnalyzer.js'
+import * as ut from './Utils.js'
 
-class Program {
+export class Program {
     // object to write to
     obj = {"data":[]};
     classMap = new Map();
     // path to save to
     relativePath = "";
     main() {
-    let test = new na.Utils();
+    let test = new ut.Utils();
     let res = test.load();
     this.obj = res[0];
     this.classMap = res[1];
 
     // call analyzers and update obj by overriding it
+        let nestingAnalyzer = new na.NestingAnalyzer();
+        this.obj = nestingAnalyzer.run(this.obj, this.classMap);
+        console.log(this.obj.data[0]);
     }
 }
 
