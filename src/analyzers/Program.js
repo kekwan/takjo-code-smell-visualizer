3a5634015944079266d18cf6a9548b7ba0143538
sqhsb <sadeqhsb@tuta.io>
2019-11-21 14:19:15 -0800

    minor fixes and update to util functions

--- src/analyzers/Program.js
+++ src/analyzers/Program.js
@@ -1,13 +1,18 @@
-import * as na from "./NestingAnalyzer.js"
+import * as na from "./Utils.js"
 
 class Program {
     // object to write to
     obj = {"data":[]};
+    classMap = new Map();
     // path to save to
     relativePath = "";
     main() {
-    let test = new na.NestingAnalyzer();
-    test.run(this.obj);
+    let test = new na.Utils();
+    let res = test.load();
+    this.obj = res[0];
+    this.classMap = res[1];
+
+    // call analyzers and update obj by overriding it
     }
 }
 
